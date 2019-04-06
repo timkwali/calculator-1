22040acad7b0494f102c260864ef9b75835bd18e
tseti <fred_luo@msn.com>
2019-04-06 17:19:43 +0200

    Add keys to index.html

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 "use strict"
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(op, a, b) {
+  
+}
