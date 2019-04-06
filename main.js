2c9660da243c7c2f3ed246689aea88a202a12418
tseti <fred_luo@msn.com>
2019-04-06 17:40:45 +0200

    Add basic arithmetic functions and eventlisteners to display numbers on screen. Changed keyboard elements from div to button

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 "use strict"
 
+let display = '';
+const numberKeys = [...document.querySelectorAll('.num')]
+const funcKeys = [...document.querySelectorAll('.op')];
+const screen = document.querySelector('.screen');
+
+addNumberListeners();
+updateScreen();
+
+function addNumberListeners() {
+  numberKeys.forEach(key => {
+    key.addEventListener('click', event => {
+      display += event.target.dataset.key;
+      updateScreen();
+    });
+  });
+}
+
+function updateScreen() {
+  screen.textContent = display;
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -17,5 +38,29 @@ function divide(a, b) {
 }
 
 function operate(op, a, b) {
-  
+  const func = parseOp(op);
+
+  return func(a, b);
 }
+
+function parseOp(op) {
+  switch(op) {
+    case '+':
+      return add;
+    case '-':
+      return subtract;
+    case '*':
+      return multiply;
+    case '/':
+      return divide;
+    default:
+      throw new Error('Invalid input');
+  }
+}
+
+
+
+
+
+
+
