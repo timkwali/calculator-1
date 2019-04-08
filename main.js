0b2951431c6ef5cf66f6b56bab650cb51e6fb322
tseti <fred_luo@msn.com>
2019-04-08 02:05:55 +0200

    Add basic stack to maintain operator precedence and the square function

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
 "use strict"
 
-let display = '';
 const numberKeys = [...document.querySelectorAll('.num')]
 const funcKeys = [...document.querySelectorAll('.op')];
 const screen = document.querySelector('.screen');
+const equal = document.querySelector('#eq');
+const clear = document.querySelector('#clear');
+const precedence = ['+-', '*/', 'q'];
+
+let display = '';
+let stack = [];
+let needArg = true;
+let isNewArg = true;
 
 addNumberListeners();
+addFuncListeners();
+addEqListeners();
+addKeyboardShortcuts();
+addClearListeners();
+
+function addClearListeners() {
+  onClick(clear, () => resetState());
+}
+
+function addEqListeners() {
+  onClick(equal, () => operate(null));
+}
 
 function addNumberListeners() {
-  numberKeys.forEach(key => {
-    key.addEventListener('click', event => {
-      display += event.target.dataset.key;
-      updateScreen();
+  numberKeys.forEach(key =>
+    onClick(key, e => newArg(e.target.dataset.key)));
+}
+
+function addFuncListeners() {
+  funcKeys.forEach(key =>
+    onClick(key, e => operate(e.target.dataset.key)));
+}
+
+function addKeyboardShortcuts() {
+  document.querySelectorAll('.key').forEach(key => {
+    document.addEventListener('keydown', e => {
+      if (key.dataset.key.includes(e.key)) key.click();
     });
   });
 }
 
-function updateScreen() {
-  screen.textContent = display;
-}
 
 function add(a, b) {
   return a + b;
@@ -36,14 +61,17 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(op, a, b) {
-  const func = parseOp(op);
+function square(n) {
+  return n * n;
+}
 
-  return func(a, b);
+function operate(op) {
+  execute(insert(op));
+  isNewArg = true;
 }
 
 function parseOp(op) {
-  switch(op) {
+  switch (op) {
     case '+':
       return add;
     case '-':
@@ -52,14 +80,79 @@ function parseOp(op) {
       return multiply;
     case '/':
       return divide;
+    case 'q':
+      return square;
     default:
-      throw new Error('Invalid input');
+      throw new Error(`Invalid input: ${op}`);
   }
 }
 
+function execute(index) {
+  needArg = true;
+  for (let i = stack.length - 1; i >= index; i--) {
+    if (isNaN(stack[i])) {
+      let func = parseOp(stack[i]);
+      let range = func.length + 1;
 
+      if (i == index && range != 2) return;
 
+      if (range == 2) needArg = false;
 
+      stack.splice(i, range,
+        func(...stack.slice(i + 1, i + range)));
+    }
+  }
+}
 
+function insert(op) {
+  let index = findStackIndex(op);
+  stack.splice(index, 0, op);
+  if (needArg) stack.push(+display);
 
+  return index;
+}
 
+function findStackIndex(op) {
+  let prevOpIndex = stack.length;
+
+  for (let i = stack.length - 1; i >= 0; i--) {
+    if (isNaN(stack[i])) {
+      if (compare(op, stack[i]) > 0) break;
+      prevOpIndex = i;
+    }
+  }
+
+  return prevOpIndex;
+}
+
+function compare(op1, op2) {
+  return getPrecedence(op1) - getPrecedence(op2);
+}
+
+function getPrecedence(op) {
+  return precedence.findIndex(el => el.includes(op));
+}
+
+function updateScreen() {
+  screen.textContent = display;
+}
+
+function resetState() {
+
+}
+
+function newArg(num) {
+  if (!needArg) {
+    stack.pop();
+    needArg = true;
+  }
+
+  display = isNewArg ? num : display + num;
+
+  isNewArg = false;
+  updateScreen();
+}
+
+function onClick(elem, func) {
+  elem.addEventListener('click', func);
+}
