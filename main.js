efe731bedb3baf22c08552b2b5cd76e3b1ae72f2
tseti <fred_luo@msn.com>
2019-04-11 01:17:26 +0200

    Move display functionality to the display object. Fix bracket bug and activeElement interering with Enter keypress after click bug

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 const numberKeys = [...document.querySelectorAll('.num')]
 const funcKeys = [...document.querySelectorAll('.op')];
 const bracketKeys = [...document.querySelectorAll('.bracket')];
-const screen = document.querySelector('.main-display');
-const opDisplayBtn = document.querySelector('.operations-display');
 const equalBtn = document.querySelector('#eq');
 const deleteBtn = document.querySelector('#delete');
 const clearBtn = document.querySelector('#c');
@@ -25,7 +23,7 @@ class Operation {
   }
 }
 
-const precedence = ['+-', '*/M', '%','pE','Qrctrl+-NLsctctrl+tctrl+Cctrl+TSCTi!'];
+const precedence = ['+-', '*/M', '%', 'pE', 'QrnNLsctctrl+tctrl+Cctrl+TSCTi!'];
 const functions = {
   '+': new Operation((a, b) => a + b, `+ `),
   '-': new Operation((a, b) => a - b, `- `),
@@ -34,8 +32,8 @@ const functions = {
   'Q': new Operation((a) => a * a, (arg) => `${arg}² `),
   'r': new Operation((a) => Math.sqrt(a), (arg) => `√${arg} `),
   '%': new Operation((a, b) => (b / 100) * a, '% '),
-  'E': new Operation((a, b) => a * Math.pow(10, b),'e+ '),
-  'ctrl+-': new Operation((a) => -1 * a, (arg) => `-(${arg})`),
+  'E': new Operation((a, b) => a * Math.pow(10, b), 'e+ '),
+  'n': new Operation((a) => -1 * a, (arg) => `-(${arg})`),
   'N': new Operation((a) => a * Math.log(a), (arg) => `ln(${arg}) `),
   'L': new Operation((a) => a * Math.log10(a), (arg) => `log(${arg}) `),
   'M': new Operation((a, b) => a % b, 'mod '),
@@ -48,25 +46,20 @@ const functions = {
   'S': new Operation((a) => Math.sinh(a), (arg) => `sinh(${arg}) `),
   'C': new Operation((a) => Math.cosh(a), (arg) => `cosh(${arg}) `),
   'T': new Operation((a) => Math.tanh(a), (arg) => `tanh(${arg}) `),
-  'p': new Operation((a, b) => Math.pow(a, b), '^'),  
+  'p': new Operation((a, b) => Math.pow(a, b), '^'),
   'i': new Operation((a) => 1 / a, (arg) => `1/${arg} `),
   '!': new Operation((a) => {
-    if ([0,1].includes(a)) return 1;
+    if ([0, 1].includes(a)) return 1;
     let fact = 1;
-    for(let i=a; i > 1; i--) {
+    for (let i = a; i > 1; i--) {
       fact *= i;
     }
     return fact;
-      }, (arg) => `${arg}! `),
+  }, (arg) => `${arg}! `),
 };
 
-let display = '';
-let opDisplay = '';
+let display = createDisplay();
 let stack = createStack();
-let needArg = true;
-let isNewArg = true;
-let finished = false;
-let lastCall = {};
 
 addListeners();
 
@@ -98,41 +91,33 @@ function addModeSelectorListeners() {
 }
 
 function addClearListeners() {
-  onClick(clearBtn, () => clear());
+  onClick(clearBtn, () => display.clear());
   onClick(clearAllBtn, () => resetState());
-  onClick(deleteBtn, () => deleteOne());
+  onClick(deleteBtn, () => display.deleteOne());
 }
 
 function addEqListeners() {
-  onClick(equalBtn, () => resolve());
+  onClick(equalBtn, () => display.resolve(arg => stack.solve(+arg)));
 }
 
 function addBracketListeners() {
   bracketKeys.forEach(bracket => onClick(bracket, e => {
-    if (e.target.dataset.key == '(') {
-      stack.newContext();
-      opDisplay += ' (';
-      clear();
-    } else {
-      isNewArg = true;
-      opDisplay += ` ${+display} )`;
-      display = stack.closeContext(+display);
-      updateScreen();
-    }
+    if (e.target.dataset.key == '(') openBracket();
+    else closeBracket();
   }));
 }
 
 function addNumberListeners() {
   numberKeys.forEach(key =>
     onClick(key, e => {
-      if (finished) resetState();
-      newArg(e.target.dataset.key)
+      if (display.finished) resetState();
+      display.newArg(e.target.dataset.key)
     }));
 }
 
 function addFuncListeners() {
   funcKeys.forEach(key => onClick(key, e => {
-    if (finished) resetState();
+    if (display.finished) resetState();
     operate(e.target.dataset.key)
   }));
 }
@@ -142,6 +127,7 @@ function addKeyboardShortcuts() {
     document.addEventListener('keydown', e => {
       let press = e.ctrlKey ? `ctrl+${e.key}` : e.key;
       if (key.dataset.key == press) {
+        document.activeElement.blur(); // Avoids clicking on outlined element on Enter
         key.click();
         key.classList.toggle('key-active', true);
         setTimeout(() => key.classList.toggle('key-active', false), 100);
@@ -150,74 +136,31 @@ function addKeyboardShortcuts() {
   });
 }
 
+function onClick(elem, func) {
+  elem.addEventListener('click', func);
+}
+
+function openBracket() {
+  stack.newContext();
+  display.openBracket();
+}
+
+function closeBracket() {
+  display.closeBracket(display.result, stack.closeContext(+display.result));
+}
+
 function operate(op) {
-  const func = functions[op].f;
-  
-  if (lastCall.f && lastCall.f.length == 1 && func.length == 1) {
-    const newStr = functions[op].toString(`(${opDisplay.slice(lastCall.index)})`);
-    opDisplay = `${opDisplay.slice(0, lastCall.index)}${newStr}`;
-  } else {
-    lastCall = {f: func, index: opDisplay.length && opDisplay.length-1, op: op};
-    opDisplay += functions[op].toString(+display);
-  }
+  const funcObj = functions[op];
+  const result = funcObj.f.length == 1 ? funcObj.f(+display.result) :
+    stack.execute(stack.insert(op, +display.result));
 
-  
-  display = func.length == 1 ? func(+display) :
-  stack.execute(stack.insert(op, +display));
-    
-  isNewArg = true;
-  updateScreen();
-}
-
-function updateScreen() {
-  screen.textContent = display;
-  opDisplayBtn.textContent = opDisplay;
-}
-
-function clear() {
-  display = '0';
-  isNewArg = true;
-  updateScreen();
+  display.add(funcObj, op, result);
 }
 
 function resetState() {
+
   stack = createStack();
-  opDisplay = '';
-  finished = false;
-  lastCall = {};
-  clear();
-}
-
-function deleteOne() {
-  if (display.length > 1) {
-    display = display.slice(0, -1);
-  } else {
-    display = 0;
-    isNewArg = true;
-  }
-  updateScreen();
-}
-
-function resolve() {
-  if (finished) resetState();
-  if (!lastCall.f) return;
-  if (lastCall.f.length == 2) opDisplay += `${+display}`;
-  
-  display = stack.solve(+display);
-  updateScreen();
-  finished = true;
-}
-
-function newArg(num) {
-  if (num == '.' && display.includes('.')) return;
-  display = isNewArg ? num : display + num;
-
-  isNewArg = false;
-  updateScreen();
-}
-
-function onClick(elem, func) {
-  elem.addEventListener('click',func);
+  display.reset();
 }
 
 function createStack() {
@@ -267,7 +210,7 @@ function createStack() {
     },
 
     solve: function (arg) {
-      this.stack.push(arg);
+      this.stack.push(+arg);
       return this.execute(0);
     },
 
@@ -289,4 +232,95 @@ function createStack() {
       return result;
     }
   };
+}
+
+function createDisplay() {
+  return {
+    resultElem: document.querySelector('.main-display'),
+    operationsElem: document.querySelector('.operations-display'),
+    operations: '',
+    result: '0',
+    isNewArg: true,
+    finished: false,
+    lastCall: {},
+
+    add(funcObj, op, res) {
+      if (this.lastCall.f && this.lastCall.f.length == 1 && funcObj.f.length == 1) {
+        const newStr = funcObj.toString(`(${this.operations.slice(this.lastCall.index)})`);
+        this.operations = `${this.operations.slice(0, this.lastCall.index)}${newStr}`;
+      } else {
+        const previousArity = this.lastCall.f ? this.lastCall.f.length : -1;
+        this.lastCall = {
+          f: funcObj.f,
+          index: this.operations.length && this.operations.length - 1,
+          op: op
+        };
+
+        this.operations += previousArity == 1 && funcObj.f.length == 2 
+          ? funcObj.toString('') : funcObj.toString(this.result);
+      }
+
+      this.result = res;
+      this.isNewArg = true;
+      this.updateScreen();
+    },
+
+    resolve(resolver) {
+      if (this.finished) resetState();
+      if (!this.lastCall.f) return;
+      if (this.lastCall.f.length == 2 &&
+        display.operations.slice(-1) != ')') this.operations += `${+this.result}`;
+
+      display.operations += ' =';
+      this.result = resolver(+this.result);
+      this.updateScreen();
+      this.finished = true;
+    },
+
+    openBracket() {
+      this.operations += ' (';
+      this.clear();
+    },
+
+    closeBracket(arg, res) {
+      this.isNewArg = true;
+      this.operations += ` ${+arg} )`;
+      this.result = res;
+      this.updateScreen();
+    },
+
+    newArg(num) {
+      if (num == '.' && this.result.includes('.')) return;
+      this.result = this.isNewArg ? num : this.result + num;
+
+      this.isNewArg = false;
+      this.updateScreen();
+    },
+
+    clear() {
+      this.result = '0';
+      this.isNewArg = true;
+      this.updateScreen();
+    },
+
+    deleteOne() {
+      if (this.result.length > 1) {
+        this.result = this.result.slice(0, -1);
+      } else {
+        this.result = 0;
+        this.isNewArg = true;
+      }
+      this.updateScreen();
+    },
+
+    reset() {
+      [this.operations, this.result, this.lastCall, this.isNewArg, this.finished] = ['', 0, {}, true, false];
+      this.updateScreen();
+    },
+
+    updateScreen() {
+      this.resultElem.textContent = this.result;
+      this.operationsElem.textContent = this.operations;
+    }
+  }
 }
